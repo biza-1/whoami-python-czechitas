9d0bc11fa6c6100ac37c4020498aee896f2f055c
biza-1 <55460297+biza-1@users.noreply.github.com>
2025-01-19 15:49:41 +0100

    increase map zoom and set default center in config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   
   # Map center coordinates
-  MAP_CENTER = [1, 0]
+  MAP_CENTER = [0, 0]
 
   # Default map zoom, range 0-18
-  MAP_ZOOM = 4
+  MAP_ZOOM = 6
